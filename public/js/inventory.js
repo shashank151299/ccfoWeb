98ef827b5cf726e585ee82ee2bac3e01b081247e
shashank151299 <sp151299@gmail.com>
2021-04-05 16:12:10 +0530

    inventory page card function done.

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -20,12 +20,31 @@ var firebaseConfig = {
       //on load of home page if any task, do it here.
       //alert('welcome ' + user.uid)
 
-      var db = firebase.database().ref('UserData/'+user.uid);
-      db.on('value', function(snapshot){
+      var db = firebase.database();
+      db.ref('UserData/'+user.uid).on('value', function(snapshot){
         document.getElementById('uname').innerHTML = snapshot.val().username;
         document.getElementById('email').innerHTML = snapshot.val().email;
       });
+      db.ref('Inventory').once('value', function(snapshot){
+        snapshot.forEach(
+          
+          function(childSnapshot){
+            var canteenId = childSnapshot.val().canteenId;
+            var category = childSnapshot.val().category;
+            var desc = childSnapshot.val().desc;
+            var imageUrl = childSnapshot.val().imageUrl;
+            var name = childSnapshot.val().name;
+            var price = childSnapshot.val().price;
+            var productId = childSnapshot.key;
 
+            db.ref('CanteenOwners/'+canteenId).on('value', function(snap){
+              var cantName = snap.val().canteenName;
+              genCard(productId,imageUrl,name,cantName,price);
+            });
+            
+          }
+        )
+      });
     } else {
       // User is not signed in.
       location.replace(link+"/login.html")
@@ -40,4 +59,61 @@ var firebaseConfig = {
       }).catch((error) => {
         // An error happened.
       });
+  }
+
+  //Card genration function.
+  function genCard(proId,imgUrl,proName,cantName,proPrice){
+    //proId is for genrating order accordinf to product
+    var mainDiv = document.getElementById('main_div');
+
+    var cardContainer = document.createElement('div');
+    cardContainer.classList.add('col-sm-3','col-6');
+
+    var cardElement = document.createElement('div');
+    cardElement.classList.add('row','inventory-card');
+
+    var foodImage =document.createElement('img');
+    foodImage.classList.add('food-img','img-fluid');
+    foodImage.alt = "food";
+    foodImage.src = imgUrl.toString();
+
+    var nameContainer = document.createElement('div');
+    nameContainer.classList.add('col-12','p-0');
+    
+    var nameElement = document.createElement('h6');
+    nameElement.className = "card-text-head";
+    nameElement.innerHTML = proName.toString();
+
+    var cantContainer = document.createElement('div');
+    cantContainer.classList.add('col-12','p-0');
+
+    var cantElement = document.createElement('p');
+    cantElement.innerHTML = cantName.toString();
+
+    var priceContainer = document.createElement('div');
+    priceContainer.classList.add('col-12','p-0');
+
+    var priceElement = document.createElement('p');
+    priceElement.innerHTML = "Price: ₹";
+
+    var priceVal = document.createElement('l');
+    priceVal.innerHTML = proPrice.toString();
+
+    var breakElement = document.createElement('br');
+
+    var repeatContainer = document.createElement('div');
+    repeatContainer.classList.add('col-12','p-0');
+
+    var repeatElement = document.createElement('a');
+    repeatElement.href = proId.toString();
+    repeatElement.innerHTML = "<img class=\"img-fluid img-cart\" src=\"res/repeat.svg\"> Add To Cart";
+
+    mainDiv.appendChild(cardContainer);
+    cardContainer.appendChild(cardElement);
+    cardElement.append(foodImage,nameContainer,cantContainer,priceContainer,repeatContainer);
+    nameContainer.appendChild(nameElement);
+    cantContainer.appendChild(cantElement);
+    priceContainer.append(priceElement,breakElement);
+    priceElement.appendChild(priceVal);
+    repeatContainer.appendChild(repeatElement);
   }
